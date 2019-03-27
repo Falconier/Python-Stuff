0f2c00016e13a9cf5b7c8f0569901d6db2bb2e9a
Jacob Emory Bullin <36140031+Falconier@users.noreply.github.com>
2019-03-27 14:03:51 -0400

    March27Afternoon7
    - finished food

--- store/food.py
+++ store/food.py
@@ -6,6 +6,9 @@ from store.product import Product
 
 
 class Food(Product):
+    def __init__(self):
+        pass
+
     def __init__(self, product, rate):
         super().__init__(product.getCode(), product.getDescription(), product.getQuantity(), product.getPrice())
         self.__taxRate = rate
@@ -13,3 +16,14 @@ class Food(Product):
     def __init__(self, code, descrip, quant, price, rate):
         super().__init__(code, descrip, quant, price)
         self.__taxRate = rate
+
+    def getRate(self):
+        return self.__taxRate
+
+    def setRate(self,r):
+        self.__taxRate = r
+
+    def __str__(self):
+        info = super().__str__()
+        info += "Sales Tax: " + str(self.__taxRate * 100) + "%\n"
+        return info
