00ffb35fcf324748a8bddda4176a15fe515342b7
Jacob Emory Bullin <36140031+Falconier@users.noreply.github.com>
2019-03-27 13:41:27 -0400

    March27Afternoon3
    - adding to product.py

---
+++ store/product.py
@@ -0,0 +1,17 @@
+##region File information
+# define product class
+# 3/27/19
+##endregion
+
+class Product:
+    def __init__(self):
+        pass
+
+    def __init__(self, code, description, quantity, price):
+        self.code = code
+        self.__description = description
+        self.__quant = quantity
+        self.price = price
+
+    def 
+
