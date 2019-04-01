7cb33ab7f3d1884881f74dd1403060fc069f7604
Jacob Emory Bullin <36140031+Falconier@users.noreply.github.com>
2019-04-01 15:42:47 -0400

    April1Afternoon3
    - turtle stuffs added
    - more things to do
    - tryed to fix some stuff

--- blackjack/SingleFileBlackjack.py
+++ blackjack/SingleFileBlackjack.py
@@ -101,27 +101,27 @@ class Dealer(Player):
             self.cards.append(deck.deal())
 
 
-class Blackjack:
-
+class Blackjack(object):
     def __init__(self):
         self.deck = Deck()
         self.deck.shuffle()
         # Pass the player and the dealer two cards each
-        self.player = Player([self.deck.deal(), self.deck.deal()])
-        self.dealer = Dealer([self.deck.deal(), self.deck.deal()])
-
+        self.player = Player([self.deck.deal(),
+        self.deck.deal()])
+        self.dealer = Dealer([self.deck.deal(),
+        self.deck.deal()])
     def play(self):
         print("Player:\n", self.player)
-
         print("Dealer:\n", self.dealer)
+        # Player hits until user says NO
         while True:
             choice = input("Do you want a hit? [y/n]: ")
             if choice in ("Y", "y"):
                 self.player.hit(self.deck.deal())
                 points = self.player.getPoints()
                 print("Player:\n", self.player)
-            if points >= 21:
-                break
+                if points >= 21:
+                    break
             else:
                 break
             playerPoints = self.player.getPoints()
@@ -133,19 +133,19 @@ class Blackjack:
                 print("Dealer:\n", self.dealer)
                 dealerPoints = self.dealer.getPoints()
                 # Determine the outcome
-            if dealerPoints > 21:
-                print("Dealer busts and you win")
-            elif dealerPoints > playerPoints:
-                print("Dealer wins")
-            elif dealerPoints < playerPoints and playerPoints <= 21:
-                print("You win")
-            elif dealerPoints == playerPoints:
-                if self.player.hasBlackjack() and not self.dealer.hasBlackjack():
-                    print("You win")
-                elif not self.player.hasBlackjack() and self.dealer.hasBlackjack():
+                if dealerPoints > 21:
+                    print("Dealer busts and you win")
+                elif dealerPoints > playerPoints:
                     print("Dealer wins")
-                else:
-                    print("There is a tie")
+                elif dealerPoints < playerPoints and playerPoints <= 21:
+                    print("You win")
+                elif dealerPoints == playerPoints:
+                    if self. player.hasBlackjack() and not self.dealer.hasBlackjack():
+                        print("You win")
+                    elif not self.player.hasBlackjack() and self.dealer.hasBlackjack():
+                        print("Dealer wins")
+                    else:
+                        print("There is a tie")
 
 
 def main():
--- turtleWorld/triangle.py
+++ turtleWorld/triangle.py
@@ -1,18 +1,25 @@
+import turtle
 from turtle import Turtle
 
 
 def draw_triangle(t, x1, y1, x2, y2, x3, y3, color):
+    t.fillcolor(color)
+    t.begin_fill()
     t.penup()
     t.goto(x1,y1)
     #move the t to x1,y1
 
     #draw the triangle
     t.pendown()
-    t.begin_fill(color)
     t.goto(x2,y2)
     t.goto(x3, y3)
     t.goto(x1, y1)
     t.end_fill()
 
 def main():
-    t = Turtle()
+    Austin = Turtle()
+
+    draw_triangle(Austin,0,0,100,0,0, -100 ,"green")
+
+    turtle.done()
+main()
